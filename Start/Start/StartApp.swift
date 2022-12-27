acf97a71a3eee5db9e068a5ccdeb308c557041d6
ryu-yeon <rytak108@gmail.com>
2022-12-28 03:10:17 +0900

    Start 생성

---
+++ Start/Start/StartApp.swift
@@ -0,0 +1,17 @@
+//
+//  StartApp.swift
+//  Start
+//
+//  Created by 유연탁 on 2022/12/28.
+//
+
+import SwiftUI
+
+@main
+struct StartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
