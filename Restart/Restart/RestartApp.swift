f698ab451c3e71e6bcd2b4854ae57f76ca1f7a26
ryu-yeon <rytak108@gmail.com>
2022-12-28 03:10:35 +0900

    Restart 생성

---
+++ Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by 유연탁 on 2022/12/28.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
