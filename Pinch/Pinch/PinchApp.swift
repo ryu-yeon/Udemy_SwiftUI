28ae9eff759ce0d61096755927101d2ae1aecf94
ryu-yeon <rytak108@gmail.com>
2022-12-28 16:18:29 +0900

    Pinch 생성

---
+++ Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by 유연탁 on 2022/12/28.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
