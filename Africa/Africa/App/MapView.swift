794789b2d12fba415f8231a8c1a8280e29c9e251
ryu-yeon <rytak108@gmail.com>
2022-12-29 07:30:47 +0900

    MapView 생성

---
+++ Africa/Africa/App/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  Africa
+//
+//  Created by 유연탁 on 2022/12/29.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("Map")
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
