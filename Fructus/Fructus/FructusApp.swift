0700297d7a9210a335d9fa6c6045824fd37fc343
ryu-yeon <rytak108@gmail.com>
2022-12-28 20:33:58 +0900

    Fructus 생성

---
+++ Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by 유연탁 on 2022/12/28.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
