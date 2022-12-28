cda15049647ca3dabcb921344d5a577e1122f735
ryu-yeon <rytak108@gmail.com>
2022-12-29 07:29:25 +0900

    GalleryView 생성

---
+++ Africa/Africa/App/GalleryView.swift
@@ -0,0 +1,20 @@
+//
+//  GalleryView.swift
+//  Africa
+//
+//  Created by 유연탁 on 2022/12/29.
+//
+
+import SwiftUI
+
+struct GalleryView: View {
+    var body: some View {
+        Text("Gallery")
+    }
+}
+
+struct GalleryView_Previews: PreviewProvider {
+    static var previews: some View {
+        GalleryView()
+    }
+}
