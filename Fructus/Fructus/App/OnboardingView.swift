f18aab4ea0768ace9e9f8858388dbcdaa86ca337
ryu-yeon <rytak108@gmail.com>
2022-12-29 02:15:52 +0900

    OnboardingView 생성

---
+++ Fructus/Fructus/App/OnboardingView.swift
@@ -0,0 +1,31 @@
+//
+//  OnboardingView.swift
+//  Fructus
+//
+//  Created by 유연탁 on 2022/12/28.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    // MARK: - PROPERTIES
+    var fruits: [Fruit] = fruitsData
+    
+    // MARK: - BODY
+    var body: some View {
+        TabView {
+            ForEach(fruits) { item in
+                FruitCardView(fruit: item)
+            } //: LOOP
+        } //: TAB
+        .tabViewStyle(PageTabViewStyle())
+        .padding(.vertical, 20)
+    }
+}
+
+// MARK: - PREVIEW
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
