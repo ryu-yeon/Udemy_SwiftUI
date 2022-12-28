b974b412aee6040e79f13c138e4b6083a85a647e
ryu-yeon <rytak108@gmail.com>
2022-12-28 20:15:40 +0900

    pagesData 생성

---
+++ Pinch/Pinch/Data/PageData.swift
@@ -0,0 +1,13 @@
+//
+//  PageData.swift
+//  Pinch
+//
+//  Created by 유연탁 on 2022/12/28.
+//
+
+import Foundation
+
+let pagesData: [Page] = [
+    Page(id: 1, imageName: "magazine-front-cover"),
+    Page(id: 2, imageName: "magazine-back-cover")
+]
