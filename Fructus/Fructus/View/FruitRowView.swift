a60a9060ecaefe3a293fa93b9dcd706806d076c7
ryu-yeon <rytak108@gmail.com>
2022-12-29 03:06:36 +0900

    FruitRowView 추가

---
+++ Fructus/Fructus/View/FruitRowView.swift
@@ -0,0 +1,48 @@
+//
+//  FruitRowView.swift
+//  Fructus
+//
+//  Created by 유연탁 on 2022/12/29.
+//
+
+import SwiftUI
+
+struct FruitRowView: View {
+    // MARK: - PROPERTIES
+    var fruit: Fruit
+    
+    // MARK: - BODY
+    var body: some View {
+        HStack {
+            Image(fruit.image)
+                .renderingMode(.original)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 80, height: 80, alignment: .center)
+                .shadow(color: .blue.opacity(0.3), radius: 3, x: 2, y: 2)
+                .background(
+                    LinearGradient(gradient: Gradient(colors: fruit.gradientColors), startPoint: .top, endPoint: .bottom)
+                )
+                .cornerRadius(8)
+            
+            VStack(alignment: .leading, spacing: 5) {
+                Text(fruit.title)
+                    .font(.title2)
+                    .fontWeight(.bold)
+                Text(fruit.headline)
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+                
+            } //: VSTACK
+        } //: HSTACK
+    }
+}
+
+// MARK: - PREVIEW
+struct FruitRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitRowView(fruit: fruitsData[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
