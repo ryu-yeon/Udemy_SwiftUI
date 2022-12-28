9297cd2d0ab7ade6ba58b670a78e80f6c6a206a7
ryu-yeon <rytak108@gmail.com>
2022-12-29 07:29:42 +0900

    VideoListView 생성

---
+++ Africa/Africa/App/VideoListView.swift
@@ -0,0 +1,20 @@
+//
+//  VideoListView.swift
+//  Africa
+//
+//  Created by 유연탁 on 2022/12/29.
+//
+
+import SwiftUI
+
+struct VideoListView: View {
+    var body: some View {
+        Text("Videos")
+    }
+}
+
+struct VideoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoListView()
+    }
+}
