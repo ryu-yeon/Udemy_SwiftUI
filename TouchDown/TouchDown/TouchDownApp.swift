d31e60ced5e000c7b6c3593913c39ab5a97f3365
ryu-yeon <rytak108@gmail.com>
2022-12-31 03:58:52 +0900

    TouchDown 생성

---
+++ TouchDown/TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by 유연탁 on 2022/12/31.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
