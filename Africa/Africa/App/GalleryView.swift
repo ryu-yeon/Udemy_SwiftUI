4e5c30b3edce444d43c68d6deb9d197056576426
ryu-yeon <rytak108@gmail.com>
2022-12-30 19:47:17 +0900

    GalleryView

--- Africa/Africa/App/GalleryView.swift
+++ Africa/Africa/App/GalleryView.swift
@@ -8,11 +8,77 @@
 import SwiftUI
 
 struct GalleryView: View {
+    // MARK: - PROPERTIES
+    
+    @State private var selectedAnimal: String = "lion"
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
+    
+    // SIMPLE GRID DEFINITION
+//    let gridLayout: [GridItem] = [
+//        GridItem(.flexible()),
+//        GridItem(.flexible()),
+//        GridItem(.flexible())
+//    ]
+    
+    //EFFICIENT GRID DEFINITION
+//    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    
+    // DYUNAMIC GRID LAYOUT
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColum: Double = 3.0
+    
+    // MARK: - FUNCTIONS
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: Int(gridColum))
+    }
+    
+    // MARK: - BODY
     var body: some View {
-        Text("Gallery")
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 30) {
+                // MARK: - IMAGE
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(.white, lineWidth: 8))
+                
+                // MARK: - SLIDER
+                Slider(value: $gridColum, in: 2...4, step: 1)
+                    .padding(.horizontal)
+                    .onChange(of: gridColum) { value in
+                        gridSwitch()
+                    }
+                
+                // MARK: - GRID
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                                haptics.impactOccurred()
+                            }
+                    } //:LOOP
+                } //: GRID
+                .animation(.easeIn, value: gridColum)
+                .onAppear {
+                    gridSwitch()
+                }
+            } //: VSTACK
+            .padding(.horizontal, 10)
+            .padding(.vertical, 50)
+        } //: SCROLL
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(MotionAnimationView())
     }
 }
 
+// MARK: - PREVIEW
 struct GalleryView_Previews: PreviewProvider {
     static var previews: some View {
         GalleryView()
