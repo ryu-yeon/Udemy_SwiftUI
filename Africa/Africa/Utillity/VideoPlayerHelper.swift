23c64af1e151dd982e607dbd6b010688381625df
ryu-yeon <rytak108@gmail.com>
2022-12-30 19:41:51 +0900

    VideoPlayerHelper 생성 playVideo 추가

---
+++ Africa/Africa/Utillity/VideoPlayerHelper.swift
@@ -0,0 +1,19 @@
+//
+//  VideoPlayerHelper.swift
+//  Africa
+//
+//  Created by 유연탁 on 2022/12/30.
+//
+
+import Foundation
+import AVKit
+
+var videoPlayer: AVPlayer?
+
+func playVideo(fileName: String, fileFormat: String) -> AVPlayer {
+    if Bundle.main.url(forResource: fileName, withExtension: fileFormat) != nil {
+        videoPlayer = AVPlayer(url: Bundle.main.url(forResource: fileName, withExtension: fileFormat)!)
+        videoPlayer?.play()
+    }
+    return videoPlayer!
+}
