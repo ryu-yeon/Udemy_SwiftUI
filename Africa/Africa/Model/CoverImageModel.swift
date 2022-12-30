1f50b28d24cd0de1519ad7648d32ce8b16dbfe5e
ryu-yeon <rytak108@gmail.com>
2022-12-30 19:42:46 +0900

    CoverImageModel 생성

---
+++ Africa/Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by 유연탁 on 2022/12/29.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
