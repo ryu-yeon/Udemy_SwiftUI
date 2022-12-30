856d2664716baa1a776240540141f79464ea1ad6
ryu-yeon <rytak108@gmail.com>
2022-12-30 19:43:26 +0900

    NationalParkLocation 생성

---
+++ Africa/Africa/Model/NationalParkLocation.swift
@@ -0,0 +1,22 @@
+//
+//  NationalParkLocation.swift
+//  Africa
+//
+//  Created by 유연탁 on 2022/12/30.
+//
+
+import Foundation
+import MapKit
+
+struct NationalParkLocation: Codable, Identifiable {
+    let id: String
+    let name: String
+    let image: String
+    let latitude: Double
+    let longitude: Double
+    
+    // Computed Property
+    var location: CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
