b9e06af888c1419c3681623722e9121e86c1e726
ryu-yeon <rytak108@gmail.com>
2022-12-30 19:47:08 +0900

    ContentView

---
+++ Africa/Africa/App/ContentView.swift
@@ -0,0 +1,108 @@
+//
+//  ContentView.swift
+//  Africa
+//
+//  Created by 유연탁 on 2022/12/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    // MARK: - PROPERTIES
+    
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
+    @State private var isGridViewActive: Bool = false
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColum: Int = 1
+    @State private var toolbarIcon: String = "square.grid.2x2"
+    
+    // MARK: - FUNCTIONS
+    func griceSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: gridLayout.count % 3 + 1)
+        gridColum = gridLayout.count
+        
+        // TOOLBAR IAMGE
+        switch gridColum {
+        case 1:
+            toolbarIcon = "square.grid.2x2"
+        case 2:
+            toolbarIcon = "square.grid.3x2"
+        case 3:
+            toolbarIcon = "rectangle.grid.1x2"
+        default:
+            toolbarIcon = "square.grid.2x2"
+        }
+    }
+    
+    // MARK: - BODY
+    var body: some View {
+        NavigationView {
+            Group {
+                if !isGridViewActive {
+                    List {
+                        CoverImageView()
+                            .frame(height: 300)
+                            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                        ForEach(animals) { animal in
+                            NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                                AnimalListItemView(animal: animal)
+                            } //: LINK
+                        } //: LOOP
+                    } //: LIST
+                } else {
+                    ScrollView(.vertical, showsIndicators: false) {
+                        LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10, content: {
+                            ForEach(animals) { animal in
+                                NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                                    AnimalGridItemView(animal: animal)
+                                } //: LINK
+                            } //: LOOP
+                        }) //: GRID
+                        .padding(10)
+                        .animation(.easeIn, value: gridColum)
+                    } //: SCROLL
+                    
+                } //: CONDITION
+            } //: GROUP
+            .listStyle(.plain)
+            .navigationBarTitle("Africa", displayMode: .large)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    HStack(spacing: 16) {
+                        // LIST
+                        Button {
+                            print("LIST")
+                            isGridViewActive = false
+                            haptics.impactOccurred()
+                        } label: {
+                            Image(systemName: "square.fill.text.grid.1x2")
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .primary : .accentColor)
+                        }
+
+                        // GRID
+                        Button {
+                            print("Grid")
+                            isGridViewActive = true
+                            haptics.impactOccurred()
+                            griceSwitch()
+                        } label: {
+                            Image(systemName: toolbarIcon)
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .accentColor : .primary)
+                        }
+
+                    } //: HSTACK
+                } //: TOOLBAR
+            }
+        } //: NAVIGATION
+    }
+}
+
+// MARK: - PREVIEW
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
