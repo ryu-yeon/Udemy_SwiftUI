224229da40dcc4626c6a96a984ff47977b3fdc35
ryu-yeon <rytak108@gmail.com>
2022-12-30 19:44:34 +0900

    AnimalGridItemView 추가

---
+++ Africa/Africa/View/AnimalGridItemView.swift
@@ -0,0 +1,29 @@
+//
+//  AnimalGridItemView.swift
+//  Africa
+//
+//  Created by 유연탁 on 2022/12/30.
+//
+
+import SwiftUI
+
+struct AnimalGridItemView: View {
+    // MARK: - PROPERTIES
+    let animal: Animal
+    
+    // MARK: - BODY
+    var body: some View {
+        Image(animal.image)
+            .resizable()
+            .scaledToFit()
+            .cornerRadius(12)
+    }
+}
+
+// MARK: - PREVIEW
+struct AnimalGridItemView_Previews: PreviewProvider {
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    static var previews: some View {
+        AnimalGridItemView(animal: animals[0])
+    }
+}
