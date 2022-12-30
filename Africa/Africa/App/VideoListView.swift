d22dbe84d57336ff10e19ccc5c6d430dab026a89
ryu-yeon <rytak108@gmail.com>
2022-12-30 19:47:00 +0900

    VideoListView

--- Africa/Africa/App/VideoListView.swift
+++ Africa/Africa/App/VideoListView.swift
@@ -8,11 +8,41 @@
 import SwiftUI
 
 struct VideoListView: View {
+    // MARK: - PROPERTIES
+    @State var videos: [Video] = Bundle.main.decode("videos.json")
+    let hapticImpact = UIImpactFeedbackGenerator(style: .medium)
+    
+    // MARK: - BODY
     var body: some View {
-        Text("Videos")
+        NavigationView {
+            List {
+                ForEach(videos) { item in
+                    NavigationLink(destination: {
+                        VideoPlayerView(videoSelected: item.id, videoTitle: item.name)
+                    }, label: {
+                        VideoListItemView(video: item)
+                            .padding(.vertical, 8)
+                    })
+                } //: LOOP
+            } //: LIST
+            .listStyle(InsetGroupedListStyle())
+            .navigationBarTitle("Videos", displayMode: .inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        // Shuffle videos
+                        videos.shuffle()
+                        hapticImpact.impactOccurred()
+                    } label: {
+                        Image(systemName: "arrow.2.squarepath")
+                    }
+                }
+            } //: NAVIGATION
+        }
     }
 }
 
+// MARK: - PREVIEW
 struct VideoListView_Previews: PreviewProvider {
     static var previews: some View {
         VideoListView()
