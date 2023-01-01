690acd2dbf3f75e47da860d4353f77f70c5ddd13
ryu-yeon <rytak108@gmail.com>
2023-01-01 16:05:57 +0900

    Devote 생성

---
+++ Devote/Devote/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Devote
+//
+//  Created by 유연탁 on 2023/01/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Devote/Devote/DevoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevoteApp.swift
+//  Devote
+//
+//  Created by 유연탁 on 2023/01/01.
+//
+
+import SwiftUI
+
+@main
+struct DevoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
